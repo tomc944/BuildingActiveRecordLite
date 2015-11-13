dbb21618b59830116a691b99523a45d9eb2f5891
Thomas Claiborne <thomas.claiborne3@gmail.com>
2015-11-13 13:29:48 -0800

    First commit

---
+++ lib/01_sql_object.rb
@@ -0,0 +1,76 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    @columns ||= DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{table_name}"
+    SQL
+
+    @columns.first.keys.map(&:to_sym)
+
+  end
+
+  def self.finalize!
+
+    columns.each do |column|
+
+      define_method("@#{column}")
+
+
+
+
+    def attributes
+      @attributes ||= {}
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.to_s.tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
