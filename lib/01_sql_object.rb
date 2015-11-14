619b5a8a3063d160f3ade6dcaea634a0e1ee23e2
Thomas Claiborne <thomas.claiborne3@gmail.com>
2015-11-13 17:01:57 -0800

    Finished SQL Object

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -1,5 +1,6 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
@@ -44,6 +45,9 @@ class SQLObject
       FROM
         "#{table_name}"
     SQL
+
+    self.parse_all(results)
+
   end
 
   def self.parse_all(results)
@@ -52,16 +56,25 @@ class SQLObject
       self.new(result)
     end
 
-    new_results.all
   end
 
   def self.find(id)
-    # ...
+    # debugger
+    found = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        "#{table_name}"
+      WHERE
+        id = ?
+    SQL
+
+    self.parse_all(found).first
   end
 
   def initialize(params = {})
     params.each do |attr_name, value|
-      if self.class.columns.include?(attr_name)
+      if self.class.columns.include?(attr_name.to_sym)
         self.send "#{attr_name}=", value
       else
         raise "unknown attribute '#{attr_name}'"
@@ -74,18 +87,50 @@ class SQLObject
   end
 
   def attribute_values
-    @attributes.values
+    values = self.class.columns.map do |col|
+      self.send "#{col}"
+    end
+
+    values
   end
 
   def insert
-    # ...
+
+    col_names = self.class.columns.join(", ")
+    question_marks = (["?"] * attribute_values.count).join(", ")
+    question_marks_string = "(#{question_marks})"
+
+    insert = DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        #{question_marks_string}
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
+
   end
 
   def update
-    # ...
+    set_line = self.class.columns.map do |col|
+      "#{col} = ?"
+    end
+
+    set_line = set_line.join(", ")
+    debugger
+
+    update = DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        id = ?
+    SQL
+
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
